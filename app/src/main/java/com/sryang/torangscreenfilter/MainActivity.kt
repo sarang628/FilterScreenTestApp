e8a650634afc7f7c3202382c9f07297e60691e10
Sarang Yang <>
2022-05-28 18:49:57 +0900

    update

--- app/src/main/java/com/sryang/torangscreenfilter/MainActivity.kt
+++ app/src/main/java/com/sryang/torangscreenfilter/MainActivity.kt
@@ -5,19 +5,27 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.torang_core.repository.FindRepository
+import com.sryang.screen_filter.FilterViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     /** 공유 뷰모델 */
-    //private val mapSharedViewModel: MapSharedViewModel by viewModels()
+    @Inject
+    lateinit var filding: FindRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btnShow).setOnClickListener {
-            //mapSharedViewModel.mapClick()
+            lifecycleScope.launch {
+                filding.clickMap()
+            }
         }
     }
 }
